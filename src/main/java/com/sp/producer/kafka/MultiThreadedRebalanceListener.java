c5179a81d6267a0d5baed4aebf16e15c9368b3e8
yushu <591821761@qq.com>
2021-01-26 09:30:35 +0800

    用于监控多线程处理

---
+++ src/main/java/com/sp/producer/kafka/MultiThreadedRebalanceListener.java
@@ -0,0 +1,72 @@
+package com.sp.producer.kafka;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * 用于监听多线程处理
+ *
+ * @author yushu.zhao
+ * @create 2020-12-25 15:36
+ *
+ */
+public class MultiThreadedRebalanceListener implements ConsumerRebalanceListener {
+
+    private final Consumer<String, String> consumer;
+    private final Map<TopicPartition, ConsumerWorker<String, String>> outstandingWorkers;
+    private final Map<TopicPartition, OffsetAndMetadata> offsets;
+
+    public MultiThreadedRebalanceListener(Consumer<String, String> consumer,
+                                          Map<TopicPartition, ConsumerWorker<String, String>> outstandingWorkers,
+                                          Map<TopicPartition, OffsetAndMetadata> offsets) {
+        this.consumer = consumer;
+        this.outstandingWorkers = outstandingWorkers;
+        this.offsets = offsets;
+    }
+
+    @Override
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        Map<TopicPartition, ConsumerWorker<String, String>> stoppedWorkers = new HashMap<>();
+        for (TopicPartition tp : partitions) {
+            ConsumerWorker<String, String> worker = outstandingWorkers.remove(tp);
+            if (worker != null) {
+                worker.close();
+                stoppedWorkers.put(tp, worker);
+            }
+        }
+
+        stoppedWorkers.forEach((tp, worker) -> {
+            long offset = worker.waitForCompletion(1, TimeUnit.SECONDS);
+            if (offset > 0L) {
+                offsets.put(tp, new OffsetAndMetadata(offset));
+            }
+        });
+
+        Map<TopicPartition, OffsetAndMetadata> revokedOffsets = new HashMap<>();
+        partitions.forEach(tp -> {
+            OffsetAndMetadata offset = offsets.remove(tp);
+            if (offset != null) {
+                revokedOffsets.put(tp, offset);
+            }
+        });
+
+        try {
+            consumer.commitSync(revokedOffsets);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+        consumer.resume(partitions);
+    }
+}
